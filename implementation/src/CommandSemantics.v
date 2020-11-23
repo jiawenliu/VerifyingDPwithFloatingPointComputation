c9db12e5238fb478b0261b725ae76d03a3c5afef
jiawenliu <jiawenliu18@gmail.com>
2020-11-23 16:51:07 -0500

    add the real sample based on the Prob.v from arthur

--- implementation/src/CommandSemantics.v
+++ implementation/src/CommandSemantics.v
@@ -50,6 +50,8 @@ Definition err : Type :=  (R * R).
 Definition distr_m := { prob env }.
 
 
+
+
 (* TODO: Be Specific *)
 Inductive  distr_e (V:Type) : Type :=
 | UNIFR: V -> V -> distr_e V
@@ -72,27 +74,35 @@ Inductive sem_distr_e (E : env): (distr_e R)
 Definition unit_E  (E : env) := dirac E.
 
 
+Definition do_sample d : {prob state} :=
+  sample: e <- d;
+ dirac e.
 
-Inductive trans_com (E : env) (delta : R)
+Definition is_sample (d: {prob state}) (E:state) : Prop :=
+  E \in supp (do_sample d).
+
+
+
+Inductive trans_com (E : env) 
   :(command) -> distr_m  -> Prop :=
 | Asgn_trans x e v er1 er2:
     trans_expr E e (v, (er1, er2)) -> 
-    trans_com E delta (ASGN (Var x) e) (unit_E ((upd E (of_nat x) (v, (er1, er2))))) (*(E & {(of_nat x) --> (v, (er1, er2)) })*)
+    trans_com E  (ASGN (Var x) e) (unit_E ((upd E (of_nat x) (v, (er1, er2)))))
 | Skip_trans:
-  trans_com E delta (SKIP) (unit_E E)
+  trans_com E  (SKIP) (unit_E E)
 | Unif01_trans x v er1 er2:
      sem_distr_e E (UNIFR 0 1) (v, (er1, er2)) ->
-     trans_com E delta (UNIF1 (Var x))
-               (unit_E (upd E (of_nat x) (v, (er1, er2)))) (*(E & { sx --> (v, (er1, er2))}) *)
+     trans_com E  (UNIF1 (Var x))
+               (unit_E (upd E (of_nat x) (v, (er1, er2))))
 | Sample_trans x v er1 er2:
      sem_distr_e E (UNIFS 1) (v, (er1, er2)) ->
-    trans_com E delta (UNIF2 (Var x))
-              (unit_E (upd E (of_nat x) (v, (er1, er2)))) (*(E & { sx --> (v, (er1, er2))}) *)
+    trans_com E  (UNIF2 (Var x))
+              (unit_E (upd E (of_nat x) (v, (er1, er2)))) 
 | Seq_trans c1 c2 E1 distr1 distr2:
-    trans_com E delta c1 distr1 ->
-     (E1 \in supp distr1) ->
-    trans_com E1 delta c2 distr2 -> 
-    trans_com E delta (SEQ c1 c2) distr2
+    trans_com E  c1 distr1 ->
+     is_sample distr1 E1 ->
+    trans_com E1  c2 distr2 -> 
+    trans_com E  (SEQ c1 c2) distr2
 .
 
 
