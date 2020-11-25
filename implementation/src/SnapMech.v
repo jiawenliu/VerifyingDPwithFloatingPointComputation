4838582219af16da7c510cb50a332afa3791e198
jiawenliu <jiawenliu18@gmail.com>
2020-11-25 16:38:46 -0500

    partial proof of the snap mech

--- implementation/src/SnapMech.v
+++ implementation/src/SnapMech.v
@@ -4,7 +4,7 @@
 From Coq
      Require Import QArith.QArith QArith.Qminmax QArith.Qabs QArith.Qreals
      micromega.Psatz Reals.Reals
-     Strings.Ascii Strings.BinaryString.
+     Strings.Ascii Strings.BinaryString Omega.
 
 From Snapv
      Require Import 
@@ -30,7 +30,7 @@ Definition Snap (a: R) (Lam: R) (B: R) (eps: R) :=
 			(Binop Clamp (Const B) 
 				(Binop Round (Const Lam)
 					(Binop Plus (Const a)
-						(Binop Mult (Const eps)
+						(Binop Mult (Const (1/eps))
 							(Binop Mult (Var 2)
 								(Unop Ln (Var 1)))))))))
 .
@@ -84,29 +84,127 @@ Admitted.
 
 Lemma SnapDP:
   forall a a' Lam B eps: R,
+     Rlt 0 Lam -> Rlt 0 B -> Rlt 0 eps->
     (Rplus a (Ropp a')) = 1 ->
     aprHore_judgement ATrue (Snap a Lam B eps) (Rmult eps (Rplus 1 (Rmult 24%R (Rmult B eta)))) (Snap a' Lam B eps)
                (fun (pm : (state * state)) =>
                     let (m1, m2) := pm in
-                    let (v1, _) := m1 (of_nat 3) in let (v2, _) := m2 (of_nat 3) in v1 = v2)
+                    let (v1, _) := m1 (of_nat 3) in let (v2, _) := m2 (of_nat 3) in
+                                                    forall v, v1 = v -> v2 = v)
 .
 
 Proof.
   intros.
+  unfold Snap.
 apply aprHore_seq with (Q := fun pm : ffun (fun=> (0, (0, 0))) * ffun (fun=> (0, (0, 0))) =>
     let (m1, m2) := pm in
-    let (v1, _) := m1 (of_nat 3) in let (v2, _) := m2 (of_nat 3) in v1 = v2)
-                       (r1 := (Rmult eps (Rplus 1 (Rmult 24%R (Rmult B eta)))))
+    let (v1, _) := m1 (of_nat 3) in let (v2, _) := m2 (of_nat 3) in forall v, v1 = v -> v2 = v)
+                       (r1 := (Rmult eps (Rplus 1 (Rmult 24%R (Rmult B eta)))) )
                        (r2 := 0%R)
                        (R0 := (fun (pm : (state * state)) =>
                                   let (m1, m2) := pm in
                                   let (v1, _) := m1 (of_nat 1) in
                                   let (v2, _) := m2 (of_nat 1) in
-                                  forall l r,
-                                    Rlt l v1 -> Rlt v1 r -> Rlt (Rmult (Rmult eps (Rplus 1 (Rmult 24%R (Rmult B eta)))) l) v2 -> Rlt v2 (Rmult (Rmult eps (Rplus 1 (Rmult 24%R (Rmult B eta)))) r))).
-apply aprHore_unifP.
-Admitted.
+                                  let (s1, _) := m1 (of_nat 2) in
+                                  let (s2, _) := m2 (of_nat 2) in
+                                  forall v,
+                                    Rlt (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a) eps) s1)) v1 ->
+                                    Rlt v1 (exp (Rdiv (Rdiv (Rminus (Rplus v Lam) a) eps) s1)) ->
+                                    Rlt (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))) v2 ->
+                                    Rlt v2 (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))))).
 
+
+apply 
+ aprHore_conseq with (P' := ATrue)
+                     (r' := (eps))
+                     (Q' :=  (fun (pm : (state * state)) =>
+                                  let (m1, m2) := pm in
+                                  let (v1, _) := m1 (of_nat 1) in
+                                  let (v2, _) := m2 (of_nat 1) in
+                                  let (s1, _) := m1 (of_nat 2) in
+                                  let (s2, _) := m2 (of_nat 2) in
+                                  forall v,
+                                    Rlt (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a) eps) s1)) v1 ->
+                                    Rlt v1 (exp (Rdiv (Rdiv (Rminus (Rplus v Lam) a) eps) s1)) ->
+                                    Rlt (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))) v2 ->
+                                    Rlt v2 (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2)))))
+                     
+.
+
+apply aprHore_unifP with (eps :=  (eps)).
+
+
+unfold assert_implies.
+auto.
+
+unfold assert_implies.
+auto.
+rewrite Rmult_plus_distr_l.
+
+rewrite Rmult_comm .
+rewrite  Rmult_1_l.
+(assert (eps = eps + 0)).
+rewrite  Rplus_0_r; auto.
+rewrite H3.
+rewrite Rplus_assoc.
+
+apply  Rplus_le_compat_l .
+
+apply Rplus_le_le_0_compat.
+apply Rle_refl.
+apply Rlt_le.
+apply Rmult_lt_0_compat .
+rewrite <- H3.
+apply H1.
+apply Rmult_lt_0_compat .
+admit.
+apply Rmult_lt_0_compat .
+apply H0.
+admit.
+
+apply
+  aprHore_seq with (Q := fun pm : ffun (fun=> (0, (0, 0))) * ffun (fun=> (0, (0, 0))) =>
+    let (m1, m2) := pm in
+    let (v1, _) := m1 (of_nat 3) in let (v2, _) := m2 (of_nat 3) in forall v, v1 = v -> v2 = v)
+                       (r1 := 0 )
+                       (r2 := 0 )
+                       (R0 := (fun (pm : (state * state)) =>
+                                  let (m1, m2) := pm in
+                                  let (v1, _) := m1 (of_nat 1) in
+                                  let (v2, _) := m2 (of_nat 1) in
+                                  let (s1, _) := m1 (of_nat 2) in
+                                  let (s2, _) := m2 (of_nat 2) in
+                                  forall v,
+                                    Rlt (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a) eps) s1)) v1 ->
+                                    Rlt v1 (exp (Rdiv (Rdiv (Rminus (Rplus v Lam) a) eps) s1)) ->
+                                    Rlt (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))) v2 ->
+                                    (Rlt v2 (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))))
+                       /\ s1 = s2)).
+apply aprHore_conseq with
+    (P' := ATrue)
+    (Q' := (fun (pm : (state * state)) =>
+                                  let (m1, m2) := pm in
+                                  let (v1, _) := m1 (of_nat 1) in
+                                  let (v2, _) := m2 (of_nat 1) in
+                                  let (s1, _) := m1 (of_nat 2) in
+                                  let (s2, _) := m2 (of_nat 2) in
+                                  forall v,
+                                    Rlt (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a) eps) s1)) v1 ->
+                                    Rlt v1 (exp (Rdiv (Rdiv (Rminus (Rplus v Lam) a) eps) s1)) ->
+                                    Rlt (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))) v2 ->
+                                    (Rlt v2 (Rmult (eps) (exp (Rdiv (Rdiv (Rminus (Rminus v Lam) a') eps) s2))))
+                                    /\ s1 = s2))
+    (r' := 0).
+
+apply aprHore_nulls with (eps := 0).
+unfold ATrue.
+unfold assert_implies; auto.
+
+unfold assert_implies; auto.
+apply Rle_refl.
+
+
+Admitted.
 
 Close Scope aprHoare_scope.
 Close Scope R_scope.
